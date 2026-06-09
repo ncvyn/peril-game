6ccb3609ab8395b15bc2b6c86b8d10d76bf84f79
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-09 17:20:50 +0000

    feat: connect server to RabbitMQ
    Add AMQP dependency
    Implement connection logic with graceful shutdown handling.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	const url = "amqp://guest:guest@localhost:5672/"
+	connection, err := amqp.Dial(url)
+	if err != nil {
+		panic(err)
+	}
+	defer connection.Close()
+
+	fmt.Println("Successfully connected to RabbitMQ server @", url)
+	waitForInterrupt()
+}
+
+func waitForInterrupt() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	<-signalChan
+	fmt.Println( /*^C*/ "opy that, shutting down the server...")
 }
